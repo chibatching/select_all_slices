edd9017ff0d84860671dd7bbf0e89331df766c1e
Takao Chiba <takao.chiba@gmail.com>
2016-08-09 23:29:13 +0900

    Rewrite plugin by javascript

--- Contents/Sketch/select_all_slices.js
+++ Contents/Sketch/select_all_slices.js
@@ -1,23 +1,23 @@
 var onRun = function(context) {
   log('Select all slice in page.');
-  
+    
   var doc = context.document
   // Unselect everything
-  [[doc currentPage] deselectAllLayers]
+  doc.currentPage().deselectAllLayers()
 
   // Recursive execute through all layers:
-  selectSliceRecursive([doc currentPage])
+  selectSliceRecursive(doc.currentPage())
 };
 
 function selectSliceRecursive(layer) {
   if (layer instanceof MSSliceLayer) {
-    [layer select:true byExpandingSelection:true]
-    log("  " + [layer name])
+    layer.select_byExpandingSelection(true, true)
+    log("  " + layer.name())
     return
   }
   try {
     var children = layer.layers()
-    for (var i = 0; i < [children length]; i++) {
+    for (var i = 0; i < children.length; i++) {
       selectSliceRecursive(children.objectAtIndex(i))
     }
   } catch(e) {
